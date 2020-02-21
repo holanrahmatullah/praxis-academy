6038aae2aa513403474e00d42fcdf2c528f38800
Efrida Marliani Harahap <efrida1700016138@webmail.uad.ac.id>
2020-02-22 04:37:36 +0700

    latihan 1 ps1

---
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/powershellscripts/Get-LastBootTime.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.SYNOPSIS
+    Shows when last your PC Started up.
+.DESCRIPTION
+    This is a WMI wrapper funtion to get the time that your PC last started up.
+.PARAMETER ComputerName
+    The name ofthe Computer you want to run the command against.
+.EXAMPLE
+    Get-LastBootTime -ComputerName Localhost
+.LINK
+    www.howtogeek.com
+#>
+param(
+    [Parameter(Mandatory=$true)][string]$ComputerName
+    )
+
+Get-WmiObject -class win32_operatingSystem -ComputerName localhost 
+Select-Object -Property CSName,@{n="Last Booted";
+e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUptime)}}
